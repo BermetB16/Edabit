4fb176c7a4cdc1c8a1af836bbbbed5bb737f14cf
BermetB <bermet160606@mail.com>
2024-12-25 22:54:52 +0600

    Even Odd Numbers

---
+++ src/main/java/easy_edabit_tasks/EvenOddNumbers.java
@@ -0,0 +1,10 @@
+package easy_edabit_tasks;
+
+public class EvenOddNumbers {
+    /*
+    */
+    public static String even_or_odd(int number) {
+        return (number % 2 == 0) ? "Even" : "Odd";
+}
+
+}
---
+++ src/test/java/EvenOddNumbers.java
@@ -0,0 +1,18 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class EvenOddNumbers {
+
+    public String even_or_odd(int number) {
+        return (number % 2 == 0) ? "Even":"Odd";
+}
+
+    @Test
+    public void evenOddNumbers(){
+        EvenOddNumbers obj = new EvenOddNumbers();
+        Assert.assertEquals(obj.even_or_odd(6), "Even");
+        Assert.assertEquals(obj.even_or_odd(7), "Odd");
+    }
+
+}
